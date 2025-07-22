e641babddb217c59ee7c7b26640eea76adff0fec
Kévin Holleville <archercelest@gmail.com>
2025-07-22 07:03:19 +0200

    mapping account

--- ArgentBank-FrontEnd/src/pages/User/User.jsx
+++ ArgentBank-FrontEnd/src/pages/User/User.jsx
@@ -1,6 +1,27 @@
 import Account from "../../components/Account/Account";
 import "./User.css";
 
+const accounts = [
+  {
+    id: 1,
+    title: "Argent Bank Checking (x8349)",
+    amount: "$2,082.79",
+    description: "Available Balance",
+  },
+  {
+    id: 2,
+    title: "Argent Bank Savings (x6712)",
+    amount: "$10,928.42",
+    description: "Available Balance",
+  },
+  {
+    id: 3,
+    title: "Argent Bank Credit Card (x8349)",
+    amount: "$184.30",
+    description: "Current Balance",
+  },
+];
+
 function User() {
   return (
     <div className="main bg-dark">
@@ -13,9 +34,15 @@ function User() {
         <button className="edit-button">Edit Name</button>
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <Account />
-      <Account />
-      <Account />
+      
+      {accounts.map((account) => (
+        <Account
+          key={account.id}
+          title={account.title}
+          amount={account.amount}
+          description={account.description}
+        />
+      ))}
     </div>
   );
 }
